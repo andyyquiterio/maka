c259136f6f6ec87101df1a9e86a0b4f123996a73
Andrea <aquiterio08@alumnos.uaq.mx>
2025-11-03 18:12:07 -0600

    proyecto

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Wedding Planner 💍</title>
+</head>
+<body>
+    <h1>Bienvenida a nuestra página de Wedding Planner</h1>
+    <p>Tu boda soñada empieza aquí ✨</p>
+</body>
+</html>
